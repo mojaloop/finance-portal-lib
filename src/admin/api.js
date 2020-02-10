1b5117d1b93e196f4cb1bfe34efe2371869917da
Kamuela Franco <kamuela.franco@gmail.com>
2020-02-10 12:03:23 +0000

    Fix linting issues

--- src/admin/api.js
+++ src/admin/api.js
@@ -4,6 +4,16 @@ const {
 } = require('../requests/requests');
 
 /**
+ * Get all participant accounts
+ *
+ * @returns {object}
+ */
+async function getParticipantAccounts(endpoint, participantName, logger) {
+    const accounts = await get(`participants/${participantName}/accounts`, { endpoint, logger });
+    return accounts;
+}
+
+/**
  * Get account details
  *
  * @returns {object}
@@ -18,28 +28,21 @@ async function getAccountById(endpoint, participantName, accountId, logger) {
     return account;
 }
 
+/**
+ * Get account details by type
+ *
+ * @returns {object}
+ */
 async function getAccountByType(endpoint, participantName, currency, accountType, logger) {
     const accounts = await getParticipantAccounts(endpoint, participantName, logger);
     const account = accounts
         .filter((a) => a.currency === currency)
         .find((ac) => ac.ledgerAccountType === accountType);
     if (account === undefined) {
-        throw new Error(`Couldn't find account with id ${accountId}`);
+        throw new Error(`Couldn't find account with type: ${accountType}`);
     }
     return account;
 }
-
-
-/**
- * Get all participant accounts
- *
- * @returns {object}
- */
-async function getParticipantAccounts(endpoint, participantName, logger) {
-    const accounts = await get(`participants/${participantName}/accounts`, { endpoint, logger });
-    return accounts;
-}
-
 
 /**
  * Get a single participant's information
@@ -47,9 +50,9 @@ async function getParticipantAccounts(endpoint, participantName, logger) {
  * @returns {object}
  */
 async function getParticipant(endpoint, participantName, logger) {
-    return await get(`participants/${participantName}`, { endpoint, logger });
+    const result = await get(`participants/${participantName}`, { endpoint, logger });
+    return result;
 }
-
 
 /**
  * Get the list of all participants
@@ -57,7 +60,8 @@ async function getParticipant(endpoint, participantName, logger) {
  * @returns {object}
  */
 async function getParticipants(endpoint, logger) {
-    return await get('participants', { endpoint, logger });
+    const result = await get('participants', { endpoint, logger });
+    return result;
 }
 
 /**
@@ -77,9 +81,9 @@ async function getParticipantEmailAddresses(endpoint, participantName, logger) {
  */
 async function updateEmailAddress(endpoint, dfspName, emailType, newEmailAddress, logger) {
     const newEndPoint = { type: emailType, value: newEmailAddress };
-    return await post(buildUrl('participants', dfspName, 'endpoints'), newEndPoint, { endpoint, logger });
+    const result = await post(buildUrl('participants', dfspName, 'endpoints'), newEndPoint, { endpoint, logger });
+    return result;
 }
-
 
 /**
  * Get net debit cap for a given dfsp+currency
@@ -104,7 +108,6 @@ async function getNDC(endpoint, dfspName, currency, logger) {
     return ndc;
 }
 
-
 /**
  * Set net debit cap for a given dfsp+currency
  *
@@ -113,9 +116,9 @@ async function getNDC(endpoint, dfspName, currency, logger) {
 async function setNDC(endpoint, dfspName, currency, newAmount, logger) {
     const ndc = await getNDC(endpoint, dfspName, currency);
     const newNDC = { currency, limit: { ...ndc.limit, value: newAmount } };
-    return await put(buildUrl('participants', dfspName, 'limits'), newNDC, { endpoint, logger });
+    const result = await put(buildUrl('participants', dfspName, 'limits'), newNDC, { endpoint, logger });
+    return result;
 }
-
 
 /**
  * Get participant from account ID
@@ -132,25 +135,15 @@ async function getParticipantByAccountId(endpoint, accountId, logger) {
     return fsp;
 }
 
-
 /**
  * Prepare DFSP funds out for reconciliation
  *
  * @returns {object}
  */
-async function fundsOutPrepareReserve(endpoint, accountId, amount, currency, reason, logger) {
-    // TODO: should really take the participant as argument
-    const fsp = await getParticipantByAccountId(endpoint, accountId);
-    return await participantFundsOutPrepareReserve(endpoint, fsp.name, accountId, amount, currency, reason, logger);
-}
-
-
-/**
- * Prepare DFSP funds out for reconciliation
- *
- * @returns {object}
- */
-async function participantFundsOutPrepareReserve(endpoint, participantName, accountId, amount, currency, reason, logger, { transferId = uuidv4() } = {}) {
+async function participantFundsOutPrepareReserve(
+    endpoint, participantName, accountId, amount, currency, reason, logger,
+    { transferId = uuidv4() } = {},
+) {
     // TODO: should really take the participant as argument
     await post(`participants/${participantName}/accounts/${accountId}`, {
         transferId,
@@ -165,25 +158,29 @@ async function participantFundsOutPrepareReserve(endpoint, participantName, acco
     return { transferId };
 }
 
-
 /**
- * Prepare, reserve, commit DFSP funds in for reconciliation
+ * Prepare DFSP funds out for reconciliation
  *
  * @returns {object}
  */
-async function fundsInReserve(endpoint, accountId, amount, reason, currency, logger) {
+async function fundsOutPrepareReserve(endpoint, accountId, amount, currency, reason, logger) {
     // TODO: should really take the participant as argument
     const fsp = await getParticipantByAccountId(endpoint, accountId);
-    return participantFundsInReserve(endpoint, fsp.name, accountId, amount, reason, currency, logger);
+    const result = await participantFundsOutPrepareReserve(
+        endpoint, fsp.name, accountId, amount, currency, reason, logger,
+    );
+    return result;
 }
-
 
 /**
  * Prepare, reserve, commit DFSP funds in for reconciliation
  *
  * @returns {object}
  */
-async function participantFundsInReserve(endpoint, participantName, accountId, amount, reason, currency, logger, { transferId = uuidv4() } = {}) {
+async function participantFundsInReserve(
+    endpoint, participantName, accountId, amount, reason, currency, logger,
+    { transferId = uuidv4() } = {},
+) {
     await post(`participants/${participantName}/accounts/${accountId}`, {
         transferId,
         externalReference: 'string', // TODO: something useful
@@ -198,81 +195,27 @@ async function participantFundsInReserve(endpoint, participantName, accountId, a
 }
 
 /**
+ * Prepare, reserve, commit DFSP funds in for reconciliation
+ *
+ * @returns {object}
+ */
+async function fundsInReserve(endpoint, accountId, amount, reason, currency, logger) {
+    // TODO: should really take the participant as argument
+    const fsp = await getParticipantByAccountId(endpoint, accountId);
+    return participantFundsInReserve(
+        endpoint, fsp.name, accountId, amount, reason, currency, logger,
+    );
+}
+
+/**
  * Set the isActive flag for a participant
  *
  * @returns {object}
  */
 async function setParticipantIsActiveFlag(endpoint, dfspName, value, logger) {
     const payload = { isActive: value };
-    return await put(buildUrl('participants', dfspName), payload, { endpoint, logger });
-}
-
-/**
- * Communicates with the FXP API in order to fetch the FXP rates for all available currency channels .
- *
- * @function getFxpRatesPerCurrencyChannel
- * @param {string} endpoint
- * @param {object} logger
- * @returns {object} An array of objects listing the exchange rates per currency channel, with this format:
- *  {
- *      <{string} the currency channel identifier. Format: concatenation of source and destination currencies, Example: "eurusd">: [
- *          {
- *              rate: <{int} The foreign exchange rate configured. The number of decimal points is provided in decimalRate.>,
- *              decimalRate: <{int} The number of decimal points of the rate>,
- *              startTime: <{string} The starting period during which the exchange rate can be applied to Forex transfers. Format: yyyy-MM-ddTHH:mm:ss.SSS[-HH:MM]>,
- *              endTime: <{string} The ending period up to which the exchange rate can be applied to Forex transfers. Format: yyyy-MM-ddTHH:mm:ss.SSS[-HH:MM]>,
- *              reuse: <{boolean} Whether to override the end date or not. When set to true and there is no other exchange rate valid at that point in time, it is expected that the exchange rate remains valid after the end date>
- *          }
- *      ]
- *  }
- */
-async function getFxpRatesPerCurrencyChannel(endpoint, logger) {
-    const currencyChannels = await getFxpCurrencyChannels(endpoint, logger);
-    const ratesForAllCurrencyChannels = await Promise.all(currencyChannels.map(async (currencyChannel) => await getFxpRatesForChannel(endpoint, currencyChannel, logger)));
-    const ratesPerCurrencyChannel = {};
-
-    ratesForAllCurrencyChannels.forEach((item) => {
-        if (item.rates != null) {
-            const customChannelIdentifier = buildCustomFxpChannelIdentifier(item.channel);
-
-            ratesPerCurrencyChannel[customChannelIdentifier] = buildCurrencyChannelRates(item.rates);
-        }
-    });
-
-    return ratesPerCurrencyChannel;
-}
-
-/**
- * Communicates with the FXP API in order to create the FXP rate for the specified currency channel.
- *
- * @function createFxpRateForCurrencyChannel
- * @param {string} endpoint
- * @param {string} currencyPair The currencies of the target channel, in a single concatenated
- * string with format "<source><destination>", as in this example: "eurusd".
- * @param {object} rateDetails
- * @param {object} logger
- * @returns {Promise<*>} The result from the FXP API.
- */
-async function createFxpRateForCurrencyChannel(endpoint, currencyPair, rateDetails, logger) {
-    const sourceCurrency = extractSourceCurrency(currencyPair);
-    const destinationCurrency = extractDestinationCurrency(currencyPair);
-    const body = {
-        rate: rateDetails.rate,
-        decimalRate: rateDetails.decimalRate,
-        startTime: rateDetails.startTime,
-        endTime: rateDetails.endTime,
-        reuse: rateDetails.reuse,
-    };
-
-    const currencyChannels = await getFxpCurrencyChannels(endpoint, logger);
-    const targetChannel = currencyChannels.find((currencyChannel) => currencyChannel.sourceCurrency.toLowerCase() === sourceCurrency.toLowerCase()
-        && currencyChannel.destinationCurrency.toLowerCase() === destinationCurrency.toLowerCase());
-
-    if (targetChannel == null) {
-        throw new Error('FXP API error - Currency channel not found.');
-    }
-
-    return await post(`exchange-rates/channels/${targetChannel.id}`, body, { endpoint, logger });
+    const result = await put(buildUrl('participants', dfspName), payload, { endpoint, logger });
+    return result;
 }
 
 /**
@@ -283,7 +226,8 @@ async function createFxpRateForCurrencyChannel(endpoint, currencyPair, rateDetai
  * @returns {Promise<*>}
  */
 async function getFxpCurrencyChannels(endpoint, logger) {
-    return await get('exchange-rates/channels', { endpoint, logger });
+    const result = await get('exchange-rates/channels', { endpoint, logger });
+    return result;
 }
 
 /**
@@ -310,7 +254,8 @@ async function getFxpRatesForChannel(endpoint, channel, logger) {
  * @returns {string}
  */
 function buildCustomFxpChannelIdentifier(fxpCurrencyChannel) {
-    return fxpCurrencyChannel.sourceCurrency.toLowerCase() + fxpCurrencyChannel.destinationCurrency.toLowerCase();
+    return fxpCurrencyChannel.sourceCurrency.toLowerCase()
+        + fxpCurrencyChannel.destinationCurrency.toLowerCase();
 }
 
 /**
@@ -327,6 +272,59 @@ function buildCurrencyChannelRates(rates) {
         endTime: item.endTime,
         reuse: item.reuse,
     }));
+}
+
+/**
+ * Communicates with the FXP API in order to fetch the FXP rates for all available currency
+ * channels.
+ *
+ * @function getFxpRatesPerCurrencyChannel
+ * @param {string} endpoint
+ * @param {object} logger
+ * @returns {object} An array of objects listing the exchange rates per currency channel, formatted:
+ *  {
+ *      <{string} the currency channel identifier. Format: concatenation of source and destination
+ *          currencies, Example: "eurusd">: [
+ *          {
+ *              rate: <{int} The foreign exchange rate configured. The number of decimal points is
+ *                  provided in decimalRate.>,
+ *              decimalRate: <{int} The number of decimal points of the rate>,
+ *              startTime: <{string} The starting period during which the exchange rate can be
+ *                  applied to Forex transfers. Format: yyyy-MM-ddTHH:mm:ss.SSS[-HH:MM]>,
+ *              endTime: <{string} The ending period up to which the exchange rate can be applied to
+ *                  Forex transfers. Format: yyyy-MM-ddTHH:mm:ss.SSS[-HH:MM]>,
+ *              reuse: <{boolean} Whether to override the end date or not. When set to true and
+ *                  there is no other exchange rate valid at that point in time, it is expected that
+ *                  the exchange rate remains valid after the end date>
+ *          }
+ *      ]
+ *  }
+ */
+async function getFxpRatesPerCurrencyChannel(endpoint, logger) {
+    const currencyChannels = await getFxpCurrencyChannels(endpoint, logger);
+    const ratesForAllCurrencyChannels = await Promise.all(
+        currencyChannels.map(
+            async (currencyChannel) => {
+                const result = await getFxpRatesForChannel(
+                    endpoint, currencyChannel, logger,
+                );
+                return result;
+            },
+        ),
+    );
+    const ratesPerCurrencyChannel = {};
+
+    ratesForAllCurrencyChannels.forEach((item) => {
+        if (item.rates != null) {
+            const customChannelIdentifier = buildCustomFxpChannelIdentifier(item.channel);
+
+            ratesPerCurrencyChannel[customChannelIdentifier] = buildCurrencyChannelRates(
+                item.rates,
+            );
+        }
+    });
+
+    return ratesPerCurrencyChannel;
 }
 
 /**
@@ -349,6 +347,41 @@ function extractSourceCurrency(currencyPair) {
  */
 function extractDestinationCurrency(currencyPair) {
     return currencyPair.substring(3, 6);
+}
+
+/**
+ * Communicates with the FXP API in order to create the FXP rate for the specified currency channel.
+ *
+ * @function createFxpRateForCurrencyChannel
+ * @param {string} endpoint
+ * @param {string} currencyPair The currencies of the target channel, in a single concatenated
+ * string with format "<source><destination>", as in this example: "eurusd".
+ * @param {object} rateDetails
+ * @param {object} logger
+ * @returns {Promise<*>} The result from the FXP API.
+ */
+async function createFxpRateForCurrencyChannel(endpoint, currencyPair, rateDetails, logger) {
+    const sourceCurrency = extractSourceCurrency(currencyPair);
+    const destinationCurrency = extractDestinationCurrency(currencyPair);
+    const body = {
+        rate: rateDetails.rate,
+        decimalRate: rateDetails.decimalRate,
+        startTime: rateDetails.startTime,
+        endTime: rateDetails.endTime,
+        reuse: rateDetails.reuse,
+    };
+
+    const currencyChannels = await getFxpCurrencyChannels(endpoint, logger);
+    const targetChannel = currencyChannels
+        .find((currencyChannel) => currencyChannel.sourceCurrency.toLowerCase()
+            === sourceCurrency.toLowerCase()
+        && currencyChannel.destinationCurrency.toLowerCase() === destinationCurrency.toLowerCase());
+
+    if (targetChannel == null) {
+        throw new Error('FXP API error - Currency channel not found.');
+    }
+    const result = await post(`exchange-rates/channels/${targetChannel.id}`, body, { endpoint, logger });
+    return result;
 }
 
 module.exports = {
